93ca333143f46af2872a1ea054319931037ef054
Sofia <sofigava@gmail.com>
2021-04-29 20:21:55 -0500

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const bcrypt=require('bcrypt');
+const cookieSession = require('cookie-session');
 
 const app=express();
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieSession({
+    secret: 'sdfkjskjhnf',
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }))
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
@@ -19,17 +25,28 @@ var User= mongoose.model("User",schema);
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get('/register', (req,res)=>{
-    res.render('index');
+app.get('/login', (req,res)=>{
+    res.render('login');
 });
 
 
-app.post('/register', async (req,res)=>{
+app.post('/login', async (req,res)=>{
+
+    let validation=false;
 
     try{
-        const hash = await bcrypt.hash(req.body.password, 10);
-        await User.create({name:req.body.name, email:req.body.email, password:hash});
-        res.redirect('/');
+        const usuario= await User.findOne({'email':req.body.email});
+        if(usuario){
+            validation=await bcrypt.compare(req.body.password, usuario.password);
+        }
+
+        if(validation){
+            req.session.user= usuario.id;
+            const usuarios= await User.find();
+            res.render('users', {"userList":usuarios});
+        }else{
+            res.render('login', {"error":true});
+        }
 
     }catch(err){
         console.log(err);
@@ -37,16 +54,11 @@ app.post('/register', async (req,res)=>{
     
 });
 
-app.get('/', async (req,res)=>{
-    try{
-        const usuarios= await User.find();
-        res.render('users', {"userList":usuarios});
-        
 
-    }catch(err){
-        console.log(err);
-    }
+app.get('/logout', (req,res)=>{
+    res.clearCookie('user');
+    req.session = null;
+    res.redirect('/login');
 });
-
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
